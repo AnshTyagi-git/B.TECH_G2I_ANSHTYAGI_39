489ee85ee480a68036bada6e11e9847fa2e886ac
AnshTyagi-git <25114cn347@glbitm.ac.in>
2026-02-12 10:44:58 +0530

    Add files via upload

---
+++ greatest.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+int main()
+{ float x,y,z;
+    printf("PLEASE ENTER DISTINCT NUMBERS\n");
+    printf("Enter the first number:");
+    scanf("%f",&x);
+    printf("Enter the second number:");
+    scanf("%f",&y);
+    printf("Enter the third number:");
+    scanf("%f",&z);
+    if (x>y && x>z)
+    {
+        printf("First number is greatest!");
+    }
+    else if(y>z && y>x)
+    {
+        printf("Second number is greatest!");
+    }
+    else
+    {
+        printf("Third number is greatest!");
+    }
+    return 0;
+}

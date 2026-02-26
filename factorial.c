ba842d18b185c8fad343c111a50690a16ba78045
Ansh Tyagi <25114cn347@glbitm.ac.in>
2026-02-26 11:56:21 +0530

    Add files via upload

---
+++ factorial.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+int main()
+{
+    int i, n, fact;
+    printf("Enter num = ");
+    scanf("%d", &n);
+
+    i = 1;
+    fact = 1;
+    while (i <= n)
+    {
+        fact = fact * i;
+        i++;
+    }
+    printf("factorial of the number = %d", fact);
+
+    return 0;
+}

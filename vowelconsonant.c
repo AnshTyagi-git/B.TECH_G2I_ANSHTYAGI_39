37b5b18ef5e2331defbc453dc48ebe26ba6b427f
Ansh Tyagi <25114cn347@glbitm.ac.in>
2026-02-26 11:47:44 +0530

    Add files via upload

---
+++ vowelconsonant.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+int main()
+{
+    char ch;
+    printf("Enter the character : ");
+    scanf("%c", &ch);
+    if (ch >= 'a' && ch <= 'z')
+    {
+        printf("%c is an lower case alphabet.\n", ch);
+        if (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u')
+        {
+            printf("%c is a vowel.\n", ch);
+        }
+        else
+        {
+            printf("%c is a consonant.\n", ch);
+        }
+    }
+
+    else if (ch >= 'A' && ch <= 'z')
+    {
+        printf("%c is an upper case alphabet .\n", ch);
+        if (ch == 'A' || ch == 'E' || ch == 'I' || ch == 'O' || ch == 'U')
+        {
+            printf("%c is a vowel.\n", ch);
+        }
+        else
+        {
+            printf("%c is a consonant.\n", ch);
+        }
+    }
+    else
+    {
+        printf("error in input\n");
+    }
+
+    return 0;
+}
